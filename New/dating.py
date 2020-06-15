0db4eed65be2530dc16b8d15038bfb8d92841db6
azh5kn <66965869+azh5kn@users.noreply.github.com>
2020-06-15 16:09:44 -0400

    Delete dating.py

--- New/dating.py
+++
@@ -1,7 +0,0 @@
-age = input('How old are you? ')
-age = int(age)
-accepted_age = (age//2)+7
-accepted_age = str(accepted_age)
-older_accepted_age = (age*2)-13
-older_accepted_age = str(older_accepted_age)
-input('You can date people between '+accepted_age+' years old and '+older_accepted_age+' years old.')
